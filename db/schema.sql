19bb4624302ee8e348372e2af75682210bed0728
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 17:37:01 -0600

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,11 @@
--- create burgers_db database
--- use burger_db
---create burgers table with
-	-- id integer primary key autoincremen 
-	--burger_name text
-	--eaten boolean
-	--date timestamp
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name TEXT,
+	eaten BOOLEAN,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
