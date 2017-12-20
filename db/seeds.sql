9ddb7061f2fd0ae29ec4f784cbc760380d2d604e
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 17:49:03 -0600

    added seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
---insert some burgers
+INSERT INTO burgers (burger_name, eaten, date) VALUES ('ditto', TRUE, "2017-07-23");
+INSERT INTO burgers (burger_name, eaten, date) VALUES ('charizard', TRUE, "2017-12-28 12:09:55");
+INSERT INTO burgers (burger_name, eaten, date) VALUES ('Gengar', FALSE, "2017-06-02 04:00:25");
