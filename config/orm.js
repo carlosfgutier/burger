deb9a52be573094ee06958b2397f97b6697f0c3f
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 22:04:50 -0600

    added insert and update functions

--- config/orm.js
+++ config/orm.js
@@ -6,9 +6,21 @@ var orm = {
 		connection.query(queryString, {tableName}, function(err, res) {
 			console.log(results);
 		});
-	}
+	},
 
-	insertOne: function
+	insertOne: function(burgerName, boolean, timestamp) {
+		var queryString = 'INSERT INTO burgers (burger_name, eaten, date) VALUES (?, ?, ?)';
+		connection.query(queryString, [burgerName, boolean, timestamp], function(err, res) {
+			console.log(results);
+		});
+	},
+
+	updateOne: function(thingOne, valueOne, thingTwo, valueTwo) {
+		var queryString = 'UPDATE burgers SET ?? = ? WHERE ?? = ?';
+		connection.query(queryString, [thingOne, valueOne, thingTwo, valueTwo], function(err, res) {
+			console.log(results);
+		});
+	}
 };
 
 
