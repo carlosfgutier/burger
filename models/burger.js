911a01152fb471d51480989e9b5e99d4a5abb972
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 22:22:39 -0600

    set up code to call orm functions and burger export

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,26 @@
 //import orm.js
-//import burger.js
+var orm = require("../config/orm.js");
+
 //create the code that will call the ORM functions using burger specific input for the ORM.
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+
+	insertOne: function(cb) {
+		orm.insertOne("burgers", function(res) {
+			cb(res);
+		});
+	},
+
+	updateOne: function(cb) {
+		orm.updateOne("burgers", function(res) {
+			cb(res);
+		})
+	}
+};
+
 //export
+module.exports = burger;
