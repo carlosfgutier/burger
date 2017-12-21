200712342314807119779527277a2d2e85fc06b4
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 22:55:02 -0600

    added inclomplete insert and update routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,30 @@ var burger = require("../models/burger.js");
 //create app router
 var router = express.Router();
 
+//create routes and set up logic where required
+//all routes lead to /index
+
+//get/selec route
+router.get("/index", function(req, res) { //<<<<<< break down
+	burger.selectAll(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+//post/insert route
+router.post("/index", function(req, res) {
+	burger.insertOne([""]) //<<<<<<<<<<<< ????
+});
+
+//update/put route
+router.put("/index", function(req, res) {
+	burger.updateOne(//<<<<<< ???)
+});
+
 
 //export router
 module.exports = router;
