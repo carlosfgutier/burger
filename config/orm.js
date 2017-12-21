6ff214decf0531e05fca181dffd54ab69424e61e
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-20 21:21:31 -0600

    added select all fucntion

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,17 @@
+var connection = require("./connection.js");
+
+var orm = {
+	selectAll: function(tableName) {
+		var queryString = 'SELECT * FROM ??';
+		connection.query(queryString, {tableName}, function(err, res) {
+			console.log(results);
+		});
+	}
+
+	insertOne: function
+};
+
+
 //populate with methods that will get, post, and edit data 
 //selectAll
 //insertOne
