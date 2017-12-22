3ebee9ce235acbf0056ab99a699f335c4ce58523
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-21 19:49:30 -0600

    added throw error to function

--- config/orm.js
+++ config/orm.js
@@ -4,26 +4,24 @@ var orm = {
 	selectAll: function(tableName, cb) {
 		var queryString = 'SELECT * FROM ??';
 		connection.query(queryString, {tableName}, function(err, res) {
-			if(err) {
-				cb(err, null)
-
-				// do something with error
-			}
+			if (err) throw err;
 			cb(null, res);
 		});
 	},
 
-	insertOne: function(burgerName, timestamp, cb) {
+	insertOne: function(burgerName, cb) {
 		var queryString = 'INSERT INTO burgers (burger_name, eaten, date) VALUES (?, ?, ?)';
-		connection.query(queryString, [burgerName, false, timestamp], function(err, res) {
-			cb(res);
+		connection.query(queryString, [burgerName, false, Date.now()], function(err, res) {
+			if (err) throw err;
+			cb(null, res);
 		});
 	},
 
 	updateOne: function(thingOne, valueOne, thingTwo, valueTwo, cb) {
 		var queryString = 'UPDATE burgers SET ?? = ? WHERE ?? = ?';
 		connection.query(queryString, [thingOne, valueOne, thingTwo, valueTwo], function(err, res) {
-			cb(res);
+			if (err) throw err;
+			cb(null, res);
 		});
 	}
 };
