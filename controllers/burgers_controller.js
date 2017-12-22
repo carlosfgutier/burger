cb3e0b5a6d14359efbe58624bcc9eace9e6ed57d
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-22 17:31:58 -0600

    finished get function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var burger = require("../models/burger.js");
-// var orm = require("../config/orm.js");
 
 //create app router
 var router = express.Router();
@@ -12,44 +11,42 @@ router.get("/",function(req, res){
 
 //get/selec route
 router.get("/index", function(req, res) { 
-	burger.selectAll(function(err,  data) {
-		if (err) throw err;
-		var hbsObject = {
+	burger.selectAll(function(data) {
+		var burgersObject = {
 			burgers: data
 		};
-		console.log(hbsObject);
-		res.render("index", hbsObject);
+		console.log(burgersObject);
+		res.render("index", burgersObject);
 	});
-	console.log("in index");
 });
 
 //post/insert route
-router.post("/api/addBurger", function(req, res) {
-	// burger.insertOne(burger.burgerName, function(err, data) {
-	// 	burger.selectAll("burgers", function(err, data) {
+// router.post("/api/addBurger", function(req, res) {
+// 	burger.insertOne(burger.burgerName, function(err, data) {
+// 		burger.selectAll("burgers", function(err, data) {
 
-	// 	});
-	// 	res.render(); <<<<<<<<<<<< also ask about this
-	// });
+// 		});
+// 		res.render(); <<<<<<<<<<<< also ask about this
+// 	});
 
-	// burger.insertOne(burger.burgerName, function(err, data) {
-	// 	res.json({id: data.id}); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-	// });
-});
+// 	burger.insertOne(burger.burgerName, function(err, data) {
+// 		res.json({id: data.id}); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// 	});
+// });
 
 //update/put route
-router.put("/api/burgers/:id", function(req, res) {
-	var burgerId = "id = " + req.params.id
+// router.put("/api/burgers/:id", function(req, res) {
+// 	var burgerId = "id = " + req.params.id
 
-	burger.update({ eaten: TRUE }, burgerId, function(err, data) {
-		if (err) throw err;
-		if (data.changedRows == 0) {
-			return res.status(404).end();
-		} else {
-			res.status(200).end();
-		}
-	});
-});
+// 	burger.update({ eaten: TRUE }, burgerId, function(err, data) {
+// 		if (err) throw err;
+// 		if (data.changedRows == 0) {
+// 			return res.status(404).end();
+// 		} else {
+// 			res.status(200).end();
+// 		}
+// 	});
+// });
 
 //export router
 module.exports = router;
